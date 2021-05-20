2f00ebeab62e9e167fb1781952df5dd58ba9ace6
mariagluck <mariamart.berlin@gmail.com>
2021-05-20 15:02:05 +0200

    completed!

--- inheritance.js
+++ inheritance.js
@@ -15,17 +15,19 @@ class Publication {
     name;
     content;
     created;
+    id;
 
     constructor( author = null, name = null, content = null) {
         this.author = author;
         this.name = name;
         this.content = content;
         this.created = new Date();
+        this.id = Publication.id(author);
     }
-    id() { //I created an id "maker" for book entries
-        let initials = this.author.split(' ').map(function(word) { return word[0]}).join('');
+    static id(x) { //I created an id "maker" for book entries
+        let initials = x.split(' ').map(function(word) { return word[0]}).join('');
         let id = initials + "-" + (Math.floor(1000 + Math.random() * 9000));
-        console.log(`id: ${id}`);
+        return id;
     }
     author() {
         console.log(`Author is ${this.author}`);
@@ -44,7 +46,7 @@ class Publication {
 const IAllende = new Publication("Isabel Allende", "La casa de los espíritus", "The story details the life of the Trueba family, spanning four generations, and tracing the post-colonial social and political upheavals of Chile – though the country's name and the names of figures closely paralleling historical ones, such as 'the President' or 'the Poet', are never explicitly given. The story is told mainly from the perspective of two protagonists (Esteban and Alba) and incorporates elements of magical realism.");
 
 
-IAllende.id(); // id: IA-7953
+//IAllende.id(); // id: IA-7953
 console.log(IAllende);
 
 // id: IA-7953
@@ -63,53 +65,53 @@ console.log(IAllende);
 // - All blogpost have an address field (given during their creation)
 
 class Blogpost extends Publication {
-    constructor(author, edited, address) {
-        super(author);
+    constructor(author, name, content, edited, address) {
+        super(author, name, content);
         this.edited = edited;
         this.address = address;
     }
     numPosts() {
-        const postsCount = Math.floor(Math.random() * 6) + 1 ; //I will change this for a counter 
+        const postsCount = Math.floor(Math.random() * 6) + 1 ; //tells a random number of posts published
         console.log(`${this.author} has published ${postsCount} posts`);
     }
 }
 
-const blog1 = new Blogpost("July SuperBlogger", "This is what this blogpost is about, bla bla, super important stuff about blog posts!", "Kreuzberg, Berlin, DE");
+const blog1 = new Blogpost("July SuperBlogger", "Blog Post", "This is what this blogpost is about, bla bla, super important stuff about blog posts!", "edited by July SP", "Kreuzberg, Berlin, DE");
 
 console.log(blog1);
 blog1.numPosts(); //July SuperBogger has published 5 posts
 // Blogpost {
 //     author: 'July SuperBlogger',
-//     name: null,
-//     content: null,
+//     name: 'Blog Post'
+//     content: 'edited by July SP',
 //     created: 2021-05-20T12:14:17.888Z,
 //     edited: 'This is what this blogpost is about, bla bla, super important stuff about blog posts!',
 //     address: 'Kreuzberg, Berlin, DE'
 //   }
 
 
-const blog2 = new Blogpost("Luzy BoringBlogger", "Today we talk about the importance of getting bored and dealing with fustration", "NYC, USA");
+const blog2 = new Blogpost("Luzy BoringBlogger", "Blog Post", "edited by Luzy BB", "Today we talk about the importance of getting bored and dealing with fustration", "NYC, USA");
 
 console.log(blog2);
 blog2.numPosts(); // Luzy BoringBlogger has published 6 posts
 // Blogpost {
 //     author: 'Luzy BoringBlogger',
-//     name: null,
-//     content: null,
+//     name: Blog Post,
+//     content: edited by Luzy BB,
 //     created: 2021-05-20T12:14:17.888Z,
 //     edited: 'Today we talk about the importance of getting bored and dealing with fustration',
 //     address: 'NYC, USA'
 //   }
 
 
-const blog3 = new Blogpost("Tommy Lee Sunny", "About the strange life of wild bears in hivernation when there is global warming and clima change!", "Antartica DC, Antartica");
+const blog3 = new Blogpost("Tommy Lee Sunny", "Blog Post", "edited by Tommy LeeS", "About the strange life of wild bears in hivernation when there is global warming and clima change!", "Antartica DC, Antartica");
 
 console.log(blog3);
 blog3.numPosts(); // Tommy Lee Sunny has published 4 posts
 // Blogpost {
 //     author: 'Tommy Lee Sunny',
-//     name: null,
-//     content: null,
+//     name: Blog Post,
+//     content: edited by Tommy LeeS,
 //     created: 2021-05-20T12:14:17.889Z,
 //     edited: 'About the strange life of wild bears in hivernation when there is global warming and clima change!',
 //     address: 'Antartica DC, Antartica'
@@ -124,52 +126,54 @@ blog3.numPosts(); // Tommy Lee Sunny has published 4 posts
 
 class Book extends Publication {
     title;
-    constructor(author, title) {
-        super(author);
+    constructor(author, title, name, content,) {
+        super(author, name, content,);
         this.title = title;
+        this.ISBN = "ISBN" + Math.floor(10000000 + Math.random() * 90000000) + "-" + Math.floor(Math.random() * 10);
 
     }
     ISBNcode() {
         const ISBNnum = Math.floor(10000000 + Math.random() * 90000000) + "-" + Math.floor(Math.random() * 10);
-        console.log(`Book reference: ISBN${ISBNnum}`);
+        console.log(this.ISBN);
     }
         
 }
 
-const academicBook = new Book("Zygmunt Bauman", "Liquid Modernity");
+const academicBook = new Book("Zygmunt Bauman", "Liquid Modernity", "Contemporary sociology", "In this new book, Bauman examines how we have moved away from a 'heavy' and 'solid', hardware-focused modernity to a 'light' and 'liquid', software-based modernity. This passage, he argues, has brought profound change to all aspects of the human condition. ...");
 console.log(academicBook);
 academicBook.ISBNcode(); //Book reference: ISBN73323849-2
 // Book {
 //     author: 'Zygmunt Bauman',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.889Z,
+//     name: 'Contemporary sociology',
+//     content: "In this new book, Bauman examines how we have moved away from a 'heavy' and 'solid', hardware-focused modernity to a 'light' and 'liquid', software-based modernity. This passage, he argues, has brought profound change to all aspects of the human condition. ...",
+//     created: 2021-05-20T12:40:00.423Z,
 //     title: 'Liquid Modernity'
 //   }
 //   Book reference: ISBN73323849-2
 
 
-const SciFiBook = new Book("Ursula K. Le Guin", "The Dispossessed");
+const SciFiBook = new Book("Ursula K. Le Guin", "The Dispossessed", "SCI-FI", "The Dispossessed: An Ambiguous Utopia is a 1974 utopian science fiction novel by American writer Ursula K. Le Guin, set in the fictional universe of the seven novels of the Hainish Cycle, e.g. The Left Hand of Darkness." );
 console.log(SciFiBook);
 SciFiBook.ISBNcode(); //Book reference: ISBN55480383-5
 // Book {
 //     author: 'Ursula K. Le Guin',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.889Z,
+//     name: 'SCI-FI',
+//     content: 'The Dispossessed: An Ambiguous Utopia is a 1974 utopian science fiction novel by American writer Ursula K. Le Guin, set in the fictional universe of the seven novels of the Hainish Cycle, e.g. The Left Hand of Darkness.',
+//     created: 2021-05-20T12:41:49.527Z,
 //     title: 'The Dispossessed'
 //   }
 //   Book reference: ISBN55480383-5
 
 
-const ComicBook = new Book("Mariko Tamaki", "This One Summer");
+const ComicBook = new Book("Mariko Tamaki", "This One Summer", "Graphic novel", "This One Summer is a graphic novel written by Mariko Tamaki and illustrated by Jillian Tamaki published by First Second Books in 2014. It is a coming of age story about two teenage friends, Rose and Windy, during a summer in Awago, a small beach town"
+);
 console.log(ComicBook);
 ComicBook.ISBNcode(); //Book reference: ISBN51695695-4
 // Book {
 //     author: 'Mariko Tamaki',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.890Z,
+//     name: 'Graphic novel',
+//     content: 'This One Summer is a graphic novel written by Mariko Tamaki and illustrated by Jillian Tamaki published by First Second Books in 2014. It is a coming of age story about two teenage friends, Rose and Windy, during a summer in Awago, a small beach town',
+//     created: 2021-05-20T12:42:59.996Z,
 //     title: 'This One Summer'
 //   }
 //   Book reference: ISBN51695695-4
@@ -185,47 +189,46 @@ ComicBook.ISBNcode(); //Book reference: ISBN51695695-4
 class Score extends Publication {
     type;
     title;
-    constructor(author, type, title) {
-        super(author);
+    constructor(author, name, content, type, title, ) {
+        super(author, name, content);
         this.type = type;
         this.title = title;
     }
 }
 
 
-const kidsBookFan = new Score("Michael Ende", "Fantasy", "The NeverEnding Story",);
+const kidsBookFan = new Score("Michael Ende", "Children book", "To escape from his dull life and bullies at school, Bastain takes refuge in an old book store. There, he begins reading an old storybook and finds himself drawn into the mythical world of Fantasia.", "Fantasy", "The NeverEnding Story",);
 console.log(kidsBookFan);
 // Score {
 //     author: 'Michael Ende',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.890Z,
+//     name: 'Children book',
+//     content: 'To escape from his dull life and bullies at school, Bastain takes refuge in an old book store. There, he begins reading an old storybook and finds himself drawn into the mythical world of Fantasia.',
+//     created: 2021-05-20T12:38:09.038Z,
 //     type: 'Fantasy',
 //     title: 'The NeverEnding Story'
 //   }
 
-const kidsBookPippi = new Score("Astrid Lindgren", "fiction", "Pippi Langstrumpf");
+const kidsBookPippi = new Score("Astrid Lindgren", "Children book", "Pippi Longstocking is the fictional main character in an eponymous series of children's books by Swedish author Astrid Lindgren. Pippi was named by Lindgren's daughter Karin, who asked her mother for a get-well story when she was off school", "fiction", "Pippi Langstrumpf");
 console.log(kidsBookPippi);
 // Score {
 //     author: 'Astrid Lindgren',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.891Z,
+//     name: 'Children book',
+//     content: "Pippi Longstocking is the fictional main character in an eponymous series of children's books by Swedish author Astrid Lindgren. Pippi was named by Lindgren's daughter Karin, who asked her mother for a get-well story when she was off school",
+//     created: 2021-05-20T12:33:45.027Z,
 //     type: 'fiction',
 //     title: 'Pippi Langstrumpf'
 //   }
 
 
-const kidsBookMonsters = new Score("Santi Balmes", "Bedtime story", "I Will Fight Monsters for You");
+const kidsBookMonsters = new Score("Santi Balmes", "Children book", "Bedtime Story","What if an entire world of MONSTERS lived underneath your floor? Martina is having trouble sleeping because she is afraid monsters might break through the floor and bring her into the monster world where she’ll have to learn how to scare humans...", "Bedtime story", "I Will Fight Monsters for You");
 console.log(kidsBookMonsters);
 
-//   Score {
+// Score {
 //     author: 'Santi Balmes',
-//     name: null,
-//     content: null,
-//     created: 2021-05-20T12:14:17.891Z,
-//     type: 'Bedtime story',
-//     title: 'I Will Fight Monsters for You'
+//     name: 'Children book',
+//     content: 'Bedtime Story`,
+//     created: 2021-05-20T12:35:32.480Z,
+//     type: 'What if an entire world of MONSTERS lived underneath your floor? Martina is having trouble sleeping because she is afraid monsters might break through the floor and bring her into the monster world where she’ll have to learn how to scare humans...',
+//     title: 'Bedtime story'
 //   }
-
 
